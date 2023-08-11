15ac634e60194e25e878288e5a997c2abe62df85
Isaac Santacruz <Isaacsantacruz73@gmail.com>
2023-08-10 20:30:25 -0400

    index

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -2,21 +2,21 @@ import { Link, useLoaderData, Form } from "react-router-dom";
 
 function Index(props) {
   // GET THE DATA FROM OUR LOADER
-  const people = useLoaderData();
+  const bookmarks = useLoaderData();
 
   return (
     <div>
-        <h2>Create a Bookmarkd</h2>
+        <h2>Create a bookmarks</h2>
         <Form action="/create" method="post">
-            <input type="text" name="name" placeholder="Bookmarkd's name"/>
-            <input type="text" name="url" placeholder="Bookmarkd's url"/>
+            <input type="text" name="name" placeholder="bookmarks's name"/>
+            <input type="text" name="url" placeholder="bookmarks's url"/>
            
         </Form>
-      {Bookmarkd.map((Bookmarkd, index) => {
+      {bookmarks.map((bookmarks, index) => {
         return (
-          <div key={Bookmarkd._id} className="Bookmarkd">
-              <h1>{Bookmarkd.name}</h1>
-            <h3>{Bookmarkd.url}</h3>
+          <div key={bookmarks._id} className="bookmarks">
+              <h1>{bookmarks.name}</h1>
+            <h3>{bookmarks.url}</h3>
           </div>
         );
       })}
